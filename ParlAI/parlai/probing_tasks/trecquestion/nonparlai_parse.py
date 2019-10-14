5b4efd3f9e5e763e65cc30867f989aa2023cca25
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2019-10-14 17:15:51 -0400

    moved TREC processing into ParlAI

---
+++ ParlAI/parlai/probing_tasks/trecquestion/nonparlai_parse.py
@@ -0,0 +1,25 @@
+"""
+Script to process TREC Question Classification dataset into ParlAI format.
+"""
+from pathlib import Path
+
+
+# Call from ParlAI directory
+project_dir = Path(__file__).parent.parent.parent.parent
+data_dir = Path(project_dir, 'data', 'probing', 'trecquestion')
+data_path = data_dir.joinpath('train_5500.label')
+question_path = data_dir.joinpath('trecquestion.txt')
+label_path = data_dir.joinpath('labels.txt')
+
+
+data = open(data_path, 'r').readlines()
+question_file = open(question_path, 'w')
+label_file = open(label_path, 'w')
+
+
+for line in data:
+    label = line[:line.index(' ')].strip()
+    question = line[line.index(' ') + 1:].rstrip()
+
+    label_file.write(label + '\n')
+    question_file.write('text:' + question + '\tlabels: \tepisode_done:True\n')
