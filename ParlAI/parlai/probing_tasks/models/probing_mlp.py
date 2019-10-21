1952b1246671213429ed962c5385b44323d6bd29
casper <thestephencasper@gmail.com>
2019-10-20 23:50:00 -0400

    added models for probing

---
+++ ParlAI/parlai/probing_tasks/models/probing_mlp.py
@@ -0,0 +1,17 @@
+
+# This is a file made by Casper to work on. Updates will be pushed soon.
+
+# imports including torch, pickle, pandas, etc.
+
+tasks = []  # list of tasks
+
+# get saved and pickled data
+
+# network class that dynamically asjusts to the input for the data
+
+# training
+
+# testing
+
+# writing results to some csv or pkl file somewhere
+6
