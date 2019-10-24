3b68190c45e6f4b2d042e803bb292d0a974ebcb3
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2019-10-24 17:03:15 -0400

    added utils for baselines

---
+++ ParlAI/probing/utils.py
@@ -0,0 +1,52 @@
+"""General utils for probing
+"""
+
+import re
+
+import numpy as np
+import contractions
+from tqdm import tqdm
+
+
+def remove_contractions(lst):
+    """Takes a list of strings and removes contractions
+    """
+    return [contractions.fix(s) for s in lst]
+
+
+def re_tokenize(s):
+    """Input string. Return list of tokenized words
+    """
+    return re.findall(r"[\w]+[']*[\w]+|[\w]+|[.,!?;]", s)
+
+
+def load_glove(path):
+    print("Loading GloVe Model")
+    with open(path, 'r', encoding='utf8') as f:
+        embs = {}
+        for line in tqdm(f):
+            splitLine = line.split(' ')
+            word = splitLine[0]
+            embedding = np.array([float(val) for val in splitLine[1:]])
+            embs[word] = embedding
+
+    print("Done.", len(embs), " words loaded!")
+    return embs
+
+
+def encode_glove(s, glove):
+    s = contractions.fix(s)
+    words = re_tokenize(s)
+
+    emb = np.zeros(glove['hi'].shape)
+    for w in words:
+        try:
+            emb += glove[w]
+
+        except KeyError:
+            # word not found in glove
+            continue
+
+    return emb / len(words)
+
+
