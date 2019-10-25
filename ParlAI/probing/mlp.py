c5228d95ef2262821c2be2952b2518145ea96864
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2019-10-25 00:53:12 -0400

    added skorch MLP.

--- ParlAI/probing/mlp.py
+++ ParlAI/probing/mlp.py
@@ -1,68 +1,18 @@
 """Implements MLP trained on embeddings for probing tasks
 """
-import torch
-from torch.utils.data import Dataset
 from torch import nn
-import torch.optim as optim
+import torch.nn.functional as F
 
+class MLP(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim=128, dropout=0.0):
+        super().__init__()
 
-class MLP():
-  def __init__(
-          self,
-          d_in,
-          d_out,
-          hidden_dim=100):
-    self.model = torch.nn.Sequential(
-        torch.nn.Linear(d_in, hidden_dim),
-        torch.nn.ReLU(),
-        torch.nn.Linear(hidden_dim, d_out),
-    )
+        self.layer1 = nn.Linear(input_dim, hidden_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.layer2 = nn.Linear(hidden_dim, output_dim)
 
-  def train(self, data, epochs=1000, log_every_n=10, split_ratio=0.2):
-    self.model.double()
-    optimizer = optim.Adam(self.model.parameters())
-    optimizer.zero_grad()
-    self.loss_fn = nn.CrossEntropyLoss()
-    num_examples = data.size()[0]
-    data = data[torch.randperm(num_examples)]  # shuffle data
-    # this is unused atm, TODO figure out how to reincorporate to avoid all the
-    # gross last col indexing
-    dataset = TrecData(data)
-    split_index = int(data.size()[0] * split_ratio)
-    test_data, train_data = data[:split_index, :], data[split_index:, :]
-    print(f'Training on {num_examples - split_index}, Testing on {split_index}')
-    for epoch in range(epochs):
-      optimizer.zero_grad()
-      trainloader = torch.utils.data.DataLoader(
-          train_data, batch_size=64, shuffle=True)
-      loss = 0
-      for example in trainloader:
-        x = example[:, :-1]
-        y = example[:, -1].long()
-        y_pred = self.model(x)
-        loss = self.loss_fn(y_pred, y)
-        loss.backward()
-        optimizer.step()
-      if epoch % 10 == 0:
-        print(f'Epoch: {epoch}')
-        print(f'Train loss: {loss}')
-        self.evaluate(test_data)
-
-  def evaluate(self, test_data):
-    y_pred = self.model(test_data[:, :-1])
-    loss = self.loss_fn(y_pred, test_data[:, -1].long())
-    print(f'Test loss: {loss}')
-
-
-class TrecData(Dataset):
-  def __init__(self, data):
-    super(TrecData, self).__init__()
-    self.x = data[:, :-1]
-    self.y = data[:, -1]
-    assert self.x.shape[0] == self.y.shape[0]
-
-  def __len__(self):
-    return self.y.shape[0]
-
-  def __getitem__(self, index):
-    return self.x[index], self.y[index].long()
+    def forward(self, X, **kwargs):
+        X = F.relu(self.layer1(X))
+        X = self.dropout(X)
+        X = F.softmax(self.layer2(X), dim=1)
+        return X
--- ParlAI/probing/probe.py
+++ ParlAI/probing/probe.py
@@ -1,35 +1,121 @@
 """Loads embeddings and evaluates their performance on a given probing task
 """
-from mlp import MLP
-import numpy as np
-import torch
-from sklearn.model_selection import train_test_split
 import pickle
+import argparse
+from pathlib import Path
+
+import numpy as np
+import torch.optim as optim
+from skorch import NeuralNetClassifier
+from skorch.callbacks import Checkpoint
+from skorch.helper import predefined_split
+from skorch.dataset import Dataset
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score
+
+from probing.mlp import MLP
 
 
-def load_trec_labels():
-  return torch.empty(5452, dtype=torch.double).random_(
-      2).unsqueeze(1)  # TODO get actual labels
+def setup_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-t', '--task', type=str, required=True,
+                        help='Usage: -t trecquestion\nOnly compatible with names in probing_tasks')
+
+    parser.add_argument('-m', '--model', type=str, required=True,
+                        help='Usage: -m GloVe\nModel directory of embeddings to be probed.' 
+                             'Assumes models saved to ParlAI\\trained')
+    return vars(parser.parse_args())
 
 
-def load_trec_data():
-  data = pickle.load(
-      open(
-          '../trained/dailydialog/small_default_transformer/probing/trecquestion/trecquestion.pkl',
-          'rb'))
-  return torch.from_numpy(data)
+if __name__ == '__main__':
+    opt = setup_args()
+    task_name = opt['task']
+    model = opt['model']
+
+    project_dir = Path(__file__).resolve().parent.parent
+
+    # Load embeddings
+    embeddings_path = project_dir.joinpath('trained', model, 'probing',
+                                           task_name, task_name + '.pkl')
+    print(f'Loading embeddings from {embeddings_path}')
+    X = pickle.load(open(embeddings_path, 'rb'))
+    X = X.astype(np.float32)
+
+    # Load labels
+    labels_path = project_dir.joinpath('data', 'probing', task_name, 'labels.txt')
+    print(f'Loading labels from {labels_path}')
+    labels = open(labels_path).readlines()
+    y = np.unique(labels, return_inverse=True)[1]
+    y = y.astype(np.int64)
+
+    # Load info
+    info_path = project_dir.joinpath('data', 'probing', task_name, 'info.pkl')
+    info = pickle.load(open(info_path, 'rb'))
+    n_train = info['n_train']
+
+    # Split data
+    X_train = X[:n_train]
+    y_train = y[:n_train]
+    X_test = X[n_train:]
+    y_test = y[n_train:]
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.1, stratify=y_train, random_state=1984
+    )
+
+    # Number of features, and number of classes
+    input_dim = len(X[0])
+    output_dim = len(set(y))
+
+    # Init skorch classifier
+    print('Initializing MLP!')
+    net = NeuralNetClassifier(
+        # Architecture
+        module=MLP,
+        module__input_dim=input_dim,
+        module__output_dim=output_dim,
+        module__hidden_dim=128,
+        module__dropout=0.5,
+        device='cuda',
+        # Training
+        max_epochs=50,
+        batch_size=128,
+        #callbacks=[Checkpoint(monitor='valid_acc_best')],
+        # train_split is validation data
+        train_split=predefined_split(Dataset(X_val, y_val)),
+        # Optimizer
+        optimizer=optim.Adam,
+        lr=0.001,
+        # Data
+        iterator_train__shuffle=True,
+    )
+
+    net.fit(X_train, y_train)
+
+    # Evaluate
+    preds = net.predict(X_train)
+    train_acc = accuracy_score(y_train, preds)
+
+    preds = net.predict(X_val)
+    val_acc = accuracy_score(y_val, preds)
+
+    preds = net.predict(X_test)
+    test_acc = accuracy_score(y_test, preds)
 
 
-def eval_trec():
-  X = load_trec_data()
-  labels = load_trec_labels()
-  num_labels = len(np.unique(labels))
-  print(f'num labels: {num_labels}')
-  model = MLP(300, num_labels)
-  print(f'data shape: {torch.cat((X, labels), dim=1).size()}')
-  model.train(torch.cat((X, labels), dim=1))
+    # Save results
+    results = {'test_acc': test_acc,
+               'val_acc': val_acc,
+               'train_acc': train_acc,
+               'model': model,
+               'task': task_name,
+               'architecture': str(net),
+               'history': net.history}
+
+    results_path = project_dir.joinpath('trained', model, 'probing',
+                                        task_name, 'training_results.pkl')
+    print(f'Saving training results to {results_path}')
+    pickle.dump(results, open(results_path, 'wb'))
 
 
-if __name__ == "__main__":
-  # TODO set cmd line args for different datasets and probing tasks
-  eval_trec()
+
