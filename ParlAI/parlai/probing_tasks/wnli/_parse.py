f64680d16edca0d5b4d9a00d7e9aa0a744b31a98
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2019-11-20 23:51:02 -0500

    added test data to wnli

--- ParlAI/parlai/probing_tasks/wnli/_parse.py
+++ ParlAI/parlai/probing_tasks/wnli/_parse.py
@@ -7,19 +7,20 @@ to process the data, and hence _parse.py instead of parse.py.
 import pickle
 from pathlib import Path
 import csv
-from os import mkdir
 
 
 project_dir = Path(__file__).resolve().parent.parent.parent.parent
 data_dir = Path(project_dir, 'data', 'probing', 'wnli')
-mkdir(data_dir)
 
-# Load data
+# Load raw data
 train_path = data_dir.joinpath('train.tsv')
 dev_path = data_dir.joinpath('dev.tsv')
+test_path = data_dir.joinpath('test.tsv')
 
 train_data = csv.DictReader(open(train_path, 'r'), dialect='excel-tab')
 dev_data = csv.DictReader(open(dev_path, 'r'), dialect='excel-tab')
+test_data = csv.DictReader(open(test_path, 'r'), dialect='excel-tab')
+all_data = train_data + dev_data + test_data
 
 # Save files
 question_path = data_dir.joinpath('wnli.txt')
@@ -32,22 +33,23 @@ info_file = open(info_path, 'wb')
 
 
 # Process data
-def process_examples(data):
-  example_count = 0
-  for example in train_data:
-    label_file.write(str(example['label']) + '\n')
-    question_file.write(
-        f"text:{example['sentence1']}\n{example['sentence2']}\tlabels:\tepisode_done:True\n")
-    example_count += 1
-  return example_count
+def process_split(data):
+    count = 0
+    for example in data:
+        label_file.write(str(example['label']) + '\n')
+        question_file.write(
+            f"text:{example['sentence1']}\n{example['sentence2']}\tlabels: \tepisode_done:True\n")
+        count += 1
+    return count
 
 
-train_data_len = process_examples(train_data)
-dev_data_len = process_examples(dev_data)
-
+train_data_len = process_split(train_data)
+dev_data_len = process_split(dev_data)
+test_data_len = process_split(test_data)
 
 # Save data info
 info = {'n_train': train_data_len,
-        'n_dev': dev_data_len}
+        'n_dev': dev_data_len,
+        'n_test': test_data_len}
 
 pickle.dump(info, info_file)
