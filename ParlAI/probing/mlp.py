eb426a0ccf318d015f7c241231a80c4476ffaef9
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2019-11-22 14:00:19 -0500

    fixed wnli dev bug and added binary MLP

--- ParlAI/probing/mlp.py
+++ ParlAI/probing/mlp.py
@@ -3,10 +3,11 @@
 from torch import nn
 import torch.nn.functional as F
 
+
 class MLP(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim=128, dropout=0.0):
         super().__init__()
-
+        self.output_dim = output_dim
         self.layer1 = nn.Linear(input_dim, hidden_dim)
         self.dropout = nn.Dropout(dropout)
         self.layer2 = nn.Linear(hidden_dim, output_dim)
@@ -14,5 +15,9 @@ class MLP(nn.Module):
     def forward(self, X, **kwargs):
         X = F.relu(self.layer1(X))
         X = self.dropout(X)
-        X = F.softmax(self.layer2(X), dim=1)
-        return X
+        if self.output_dim == 1:
+            X = self.layer2(X)
+            return X.squeeze(-1)
+        else:
+            X = F.softmax(self.layer2(X), dim=1)
+            return X
