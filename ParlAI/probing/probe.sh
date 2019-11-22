036447c18b33ed38d80541ca5be8d6b4d384e8f3
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2019-11-22 14:34:31 -0500

    probe.sh with if else

--- ParlAI/probing/probe.sh
+++ ParlAI/probing/probe.sh
@@ -7,12 +7,20 @@
 # MF=
 # TASK=
 
-MODEL_DIRS = `ls ../trained/dailydialog`
-for DIR in $MODEL_DIRS
-if "DIR" -ne "old"
+MODEL_DIRS=`ls ../trained/dailydialog`
+for dir in $MODE_DIRS
 do
-  echo "$dir"
-done
+if [ $dir != 'old' ]
+then
+    if [[ $dir == *'transformer'* ]]
+    then
+    echo "$dir"
 
+    if [[ $dir == *'seq2seq'* ]]
+    then
+    echo "$dir"
+    fi
+fi
+done
 
 # CUDA_VISIBLE_DEVICES=$CUDA python examples/eval_model.py -t $TASK -mf $MF --batchsize $BATCH --probe True
