ff0e5368a24d12bf82b0e99e2ff78213bfff13e8
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2019-11-22 14:20:28 -0500

    listing model files with bash script

--- ParlAI/probing/probe.sh
+++ ParlAI/probing/probe.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo Hello, World!
+# TASK_NAME=$1
+# CUDA=$2
+# BATCH=$3
+#
+# MF=
+# TASK=
+
+MODEL_DIRS = `ls ../trained/dailydialog`
+for DIR in $MODEL_DIRS
+if "DIR" -ne "old"
+do
+  echo "$dir"
+done
+
+
+# CUDA_VISIBLE_DEVICES=$CUDA python examples/eval_model.py -t $TASK -mf $MF --batchsize $BATCH --probe True
