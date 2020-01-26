ed9b80f4629474fdc30ebbafe8e7fc79f1179813
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2020-01-26 16:11:50 -0500

    commented out unnecessary(?) if conditions

--- probing/probe.sh
+++ probing/probe.sh
@@ -11,21 +11,22 @@ for dir in $dirs
 do
 if [ $dir != 'old' ]
 then
-    if [[ $dir == *'transformer'* ]]
-    then
+#    if [[ $dir == *'transformer'* ]]
+#    then
+#    m="dailydialog/${dir}"
+#    # echo "$mf"
+#
+#    elif [[ $dir == *'seq2seq_att'* ]]
+#    then
+#    m="dailydialog/${dir}"
+#    # echo "$mf"
+#
+#    elif [[ $dir == *'seq2seq'* ]]
+#    then
+#    m="dailydialog/${dir}"
+#    # echo "$mf"
+#    fi
     m="dailydialog/${dir}"
-    # echo "$mf"
-
-    elif [[ $dir == *'seq2seq_att'* ]]
-    then
-    m="dailydialog/${dir}"
-    # echo "$mf"
-
-    elif [[ $dir == *'seq2seq'* ]]
-    then
-    m="dailydialog/${dir}"
-    # echo "$mf"
-    fi
     command="CUDA_VISIBLE_DEVICES=${CUDA} python probing/probe.py -t ${TASK_NAME} -m ${m} -ep ${EPOCHS}"
     eval "$command"
 fi
