94e6a3212f0790261e18809e0689bdf602c3c51c
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2020-02-14 01:00:24 -0500

    updated bash scripts to cover wikipedia models

--- probing/eval.sh
+++ probing/eval.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
+# Example usage bash probing/eval.sh trecquestion 0 1024 wikitext-103
 TASK_NAME=$1
 CUDA=$2
 BATCH=$3
+DATASET=$4
 
 task="parlai.probing_tasks.${TASK_NAME}.agents"
 
-dirs=`ls trained/dailydialog`
+dirs=`ls trained/${DATASET}`
 for dir in $dirs
 do
 if [ $dir != 'old' ]
 then
     if [[ $dir == *'transformer'* ]]
     then
-    mf="trained/dailydialog/${dir}/transformer"
+    m="transformer"
     # echo "$mf"
 
     elif [[ $dir == *'seq2seq_att'* ]]
     then
-    mf="trained/dailydialog/${dir}/seq2seq_att"
+    m="seq2seq_att"
     # echo "$mf"
 
     elif [[ $dir == *'seq2seq'* ]]
     then
-    mf="trained/dailydialog/${dir}/seq2seq"
+    m="seq2seq"
     # echo "$mf"
     fi
+    mf="trained/${DATASET}/${dir}/${m}"
     command="CUDA_VISIBLE_DEVICES=${CUDA} python examples/eval_model.py -t ${task} -mf ${mf} --batchsize ${BATCH} --probe True"
     eval "$command"
 fi
--- probing/probe.sh
+++ probing/probe.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
+# Example usage bash probing/probe.sh trecquestion 0 50 128 0.001 128 dailydialg
 TASK_NAME=$1
 CUDA=$2
 EPOCHS=$3
+BATCHSIZE=$4
+LR=$5
+HIDDEN=$6
+DATASET=$7
 
 task="parlai.probing_tasks.${TASK_NAME}.agents"
 
-dirs=`ls trained/dailydialog`
+dirs=`ls trained/${DATASET}`
 for dir in $dirs
 do
 if [ $dir != 'old' ]
 then
-#    if [[ $dir == *'transformer'* ]]
-#    then
-#    m="dailydialog/${dir}"
-#    # echo "$mf"
-#
-#    elif [[ $dir == *'seq2seq_att'* ]]
-#    then
-#    m="dailydialog/${dir}"
-#    # echo "$mf"
-#
-#    elif [[ $dir == *'seq2seq'* ]]
-#    then
-#    m="dailydialog/${dir}"
-#    # echo "$mf"
-#    fi
-    m="dailydialog/${dir}"
-    command="CUDA_VISIBLE_DEVICES=${CUDA} python probing/probe.py -t ${TASK_NAME} -m ${m} -ep ${EPOCHS}"
+   # if [[ $dir == *'transformer'* ]]
+   # then
+   # m="dailydialog/${dir}"
+   # # echo "$mf"
+   #
+   # elif [[ $dir == *'seq2seq_att'* ]]
+   # then
+   # m="dailydialog/${dir}"
+   # # echo "$mf"
+   #
+   # elif [[ $dir == *'seq2seq'* ]]
+   # then
+   # m="dailydialog/${dir}"
+   # # echo "$mf"
+   # fi
+    m="${DATASET}/${dir}"
+    command="CUDA_VISIBLE_DEVICES=${CUDA} python probing/probe.py -t ${TASK_NAME} -m ${m} -ep ${EPOCHS} -bs ${BATCHSIZE} -lr ${LR} -hidden ${HIDDEN}"
     eval "$command"
 fi
 done
