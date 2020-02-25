422226290703422877eef3000967c2b3230bf6df
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2020-02-24 22:32:00 -0500

    added runs to bash script

--- probing/probe.sh
+++ probing/probe.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Example usage bash probing/probe.sh trecquestion 0 50 128 0.001 128
+# Example usage bash probing/probe.sh trecquestion 0 50 128 0.001 128 10
 TASK_NAME=$1
 CUDA=$2
 EPOCHS=$3
 BATCHSIZE=$4
 LR=$5
 HIDDEN=$6
+RUNS=$7
 
 task="parlai.probing_tasks.${TASK_NAME}.agents"
 
@@ -20,7 +21,7 @@ do
            for MODULE in embeddings encoder all
            do
                 m="${DATASET}/${dir}"
-                command="CUDA_VISIBLE_DEVICES=${CUDA} python probing/probe.py -t ${TASK_NAME} -p ${MODULE} -m ${m} -ep ${EPOCHS} -bs ${BATCHSIZE} -lr ${LR} -hidden ${HIDDEN}"
+                command="CUDA_VISIBLE_DEVICES=${CUDA} python probing/probe.py -t ${TASK_NAME} -p ${MODULE} -m ${m} -ep ${EPOCHS} -r ${RUNS} -bs ${BATCHSIZE} -lr ${LR} -hidden ${HIDDEN}"
                 eval "$command"
 #                echo "$command"
            done
