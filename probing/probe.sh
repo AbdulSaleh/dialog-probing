c157b79c191934e88312d59927a7ab14172c6692
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2020-04-02 23:41:03 -0400

    updated probe.sh to evaluate hierarchical embs

--- probing/probe.sh
+++ probing/probe.sh
@@ -28,14 +28,12 @@ do
                 continue
             fi
 
-            for MODULE in "${arr[@]}"
+            for MODULE in "${modules[@]}"
             do
                 m="${DATASET}/${dir}"
                 command="CUDA_VISIBLE_DEVICES=${CUDA} python probing/probe.py -t ${TASK_NAME} -p ${MODULE} -m ${m} -ep ${EPOCHS} -r ${RUNS} -bs ${BATCHSIZE} -lr ${LR} -hidden ${HIDDEN}"
                 eval "$command"
             done
-
-
 #           for MODULE in encoder_embeddings encoder_state encoder_embeddings_state
 #           do
 #                m="${DATASET}/${dir}"
