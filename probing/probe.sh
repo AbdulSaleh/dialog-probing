0d3914bfd6a80b507146e5f2256da6d177cc14a7
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2020-04-02 23:37:03 -0400

    updated probe.sh to evaluate hierarchical embs

--- probing/probe.sh
+++ probing/probe.sh
@@ -16,17 +16,34 @@ do
     dirs=`ls trained/${DATASET}`
     for dir in $dirs
     do
-    continue
         if [ $dir != 'old' ]
         then
-           for MODULE in encoder_embeddings encoder_state encoder_embeddings_state
-           do
+            if [[ $dir == *'seq2seq_att'* ]]
+            then
+                modules=("hierarchical_encoder_state" "hierarchical_encoder_embeddings_state")
+            elif [[ $dir == *'transformer'* ]]
+            then
+                modules=("encoder_embeddings" "hierarchical_encoder_state" "hierarchical_encoder_embeddings_state")
+            else
+                continue
+            fi
+
+            for MODULE in "${arr[@]}"
+            do
                 m="${DATASET}/${dir}"
                 command="CUDA_VISIBLE_DEVICES=${CUDA} python probing/probe.py -t ${TASK_NAME} -p ${MODULE} -m ${m} -ep ${EPOCHS} -r ${RUNS} -bs ${BATCHSIZE} -lr ${LR} -hidden ${HIDDEN}"
                 eval "$command"
-#                echo "$command"
-           done
-        fi
+            done
+
+
+#           for MODULE in encoder_embeddings encoder_state encoder_embeddings_state
+#           do
+#                m="${DATASET}/${dir}"
+#                command="CUDA_VISIBLE_DEVICES=${CUDA} python probing/probe.py -t ${TASK_NAME} -p ${MODULE} -m ${m} -ep ${EPOCHS} -r ${RUNS} -bs ${BATCHSIZE} -lr ${LR} -hidden ${HIDDEN}"
+#                eval "$command"
+##                echo "$command"
+#           done
+         fi
     done
 done
 
