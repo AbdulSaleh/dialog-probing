aec778741290a57b84f7fe863dd9faea9b037138
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2020-06-06 17:33:45 -0400

    snips supports auto download

--- probing/probing_tasks/snips/build.py
+++ probing/probing_tasks/snips/build.py
@@ -1,5 +1,74 @@
 import os
+import json
+import pickle
+from pathlib import Path
 import parlai.core.build_data as build_data
+
+
+def download_data(dpath):
+    labels = ['AddToPlaylist', 'BookRestaurant', 'GetWeather', 'PlayMusic',
+              'RateBook', 'SearchCreativeWork', 'SearchScreeningEvent']
+
+    for label in labels:
+        build_data.make_dir(os.path.join(dpath, label))
+
+        train_url = 'https://raw.githubusercontent.com/snipsco/nlu-benchmark/'\
+                    'master/2017-06-custom-intent-engines/{}/'\
+                    'train_{}_full.json'.format(label, label)
+        test_url = 'https://raw.githubusercontent.com/snipsco/nlu-benchmark/'\
+                   'master/2017-06-custom-intent-engines/{}/'\
+                   'validate_{}.json'.format(label, label)
+
+        build_data.download(train_url, os.path.join(dpath, label), 'train.json')
+        build_data.download(test_url, os.path.join(dpath, label), 'test.json')
+
+
+def create_probing_format(orig_dpath):
+    labels = ['AddToPlaylist', 'BookRestaurant', 'GetWeather', 'PlayMusic',
+              'RateBook', 'SearchCreativeWork', 'SearchScreeningEvent']
+
+    data_dir = orig_dpath.parent
+
+    question_path = data_dir.joinpath('snips.txt')
+    label_path = data_dir.joinpath('labels.txt')
+    info_path = data_dir.joinpath('info.pkl')
+
+    question_file = open(question_path, 'w', encoding='utf-8')
+    label_file = open(label_path, 'w')
+    info_file = open(info_path, 'wb')
+
+    n_train = 0
+    n_test = 0
+
+    # Process train data
+    for label in labels:
+        f_name = orig_dpath.joinpath(label, 'train.json')
+        with open(f_name, encoding="utf-8", errors='ignore') as f:
+            dataset = json.load(f)
+            for example in dataset[label]:
+                text = ''.join([t['text'] for t in example['data']])
+                text = text.replace('\n', '').replace('\t', '')
+                question_file.write(('text:' + text + '\tepisode_done:True\n'))
+                label_file.write(label + '\n')
+                n_train += 1
+
+    # Process test data
+    for label in labels:
+        f_name = orig_dpath.joinpath(label, 'test.json')
+        with open(f_name, encoding="utf-8", errors='ignore') as f:
+            dataset = json.load(f)
+            for example in dataset[label]:
+                text = (''.join([t['text'] for t in example['data']]))
+                text = text.replace('\n', '').replace('\t', '')
+                question_file.write('text:' + text + '\tepisode_done:True\n')
+                label_file.write(label + '\n')
+                n_test += 1
+
+    # Save data info
+    info = {'n_train': n_train,
+            'n_test': n_test}
+
+    pickle.dump(info, info_file)
 
 
 def build(opt):
@@ -10,6 +79,16 @@ def build(opt):
     if not build_data.built(dpath):
         print('[building data: ' + dpath + ']')
 
+        build_data.make_dir(dpath)
+        orig_dpath = os.path.join(dpath, 'snips_orig')
+
+        download_data(orig_dpath)
+        create_probing_format(Path(orig_dpath))
+
         # mark the data as built
         build_data.mark_done(dpath)
 
+
+if __name__ == '__main__':
+    opt = {'datapath': 'C:/Users/Abdul/Workspace/media_lab/dialog-probing/data/'}
+    build(opt)
